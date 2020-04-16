ffb8ad44947492a279e62943e71878c1ce070c01
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-04-16 13:17:56 -0400

    Check git ignore file. Move layout image to assets folder.

--- src/Containers/Layout.js
+++ src/Containers/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import { Layout, Menu } from 'antd';
+import logo from '../Assets/movie.png';
 import "antd/dist/antd.css";
 
 const { Header } = Layout;
@@ -11,7 +12,7 @@ class CustomLayout extends Component{
             <Layout className="layout">
                 <Header>
                     <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
-                        <Menu.Item><Link to="/"><img alt="" src="movie.png" height="40px" /></Link></Menu.Item>
+                        <Menu.Item><Link to="/"><img alt="Movie" src={logo} height="40px" /></Link></Menu.Item>
                         <Menu.Item key="1"><Link to="/">Buscador de Películas</Link></Menu.Item>
                         <Menu.Item key="2"><Link to="/favorite-movies">Favoritas</Link></Menu.Item>
                     </Menu>
