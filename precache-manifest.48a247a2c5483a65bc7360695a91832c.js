69f24f9a4ccfd84501c543f03e3d6908d7ac9362
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-04-16 16:41:16 -0400

    Updates

---
+++ precache-manifest.48a247a2c5483a65bc7360695a91832c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e0925bbd2c5f8967533164cb7e4bed",
+    "url": "/Movie-Finder/index.html"
+  },
+  {
+    "revision": "41e55e54ac1e060268bb",
+    "url": "/Movie-Finder/static/css/2.29c1ed91.chunk.css"
+  },
+  {
+    "revision": "e686c98a1de258ec1b26",
+    "url": "/Movie-Finder/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41e55e54ac1e060268bb",
+    "url": "/Movie-Finder/static/js/2.6d4b5f7d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Movie-Finder/static/js/2.6d4b5f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e686c98a1de258ec1b26",
+    "url": "/Movie-Finder/static/js/main.17f4a901.chunk.js"
+  },
+  {
+    "revision": "08d25ff02e81b4969be8",
+    "url": "/Movie-Finder/static/js/runtime-main.7b12f992.js"
+  },
+  {
+    "revision": "af39d2321b95c3717ca9b99c61cc142f",
+    "url": "/Movie-Finder/static/media/movie.af39d232.png"
+  }
+]);
